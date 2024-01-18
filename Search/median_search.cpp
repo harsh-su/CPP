72001d96e39733abd90101cd9ea3ea4ada7efd1b
harsh-su <harshsoa642@gmail.com>
2024-01-18 23:37:13 +0530

    Added median_of_medians : kth smallest elemnt finding algo .also included a good link for learnign the complexity

--- Search/median_search.cpp
+++ Search/median_search.cpp
@@ -7,59 +7,66 @@
 median of median is an approximate median selection aglorithm frequtnly used as a good pivot for an exact selection algorithm most commoly quicksort -
 ->O(n) time 
 quicksort -> slects kth smallest element of an initally unsorted array 
+
+good linking for studying why its imp
+-> https://rcoh.me/posts/linear-time-median-finding/
 */
 
 void print_arr(const std::vector<int> &m){
     int n = m.size();
     for (int i=0;i<n;i++){
-        std::cout << m[i] <<std::endl; 
+        std::cout << m[i] << " "; 
     }
+    std::cout << std::endl;
 }
 
-int  median_of_median(const std::vector<int> &arr , const int& idx){
-    int pivot  =0;
+int median_of_medians(const std::vector<int> &arr , const int& idx){
+    int pivot  = 0;
     std::vector<int> a(arr.begin(),arr.end());
-    std::vector<int>m;
+    std::vector<int> m;
 
-    int r = a.size();
-    for (int i=0;i<r;i+=5){
-        std::sort(a.begin()+i,a.begin()+std::min(r,i+5));
-        int mid = (i+std::min(r,i+5))/2;
+    int sz = a.size();
+    for (int i =0;i<sz;i+=5){
+        std::sort(a.begin()+i,a.begin()+std::min(sz,i+5));
+        int mid  = (i+std::min(sz,i+5))/2;
         m.push_back(a[mid]);
     }
 
-    print_arr(m);
-
-    int sz = int (m.size());
-    if (sz <=5){
+    int m_sz = int(m.size());
+    if(m_sz <= 5){
         std::sort(m.begin(),m.end());
-        pivot = m[(sz -1) /2];
+        pivot = m[(m_sz -1)/2];
+
     }else {
-        pivot  = median_of_median(m,idx);
+        pivot = median_of_medians(m,idx);
     }
 
     std::vector<int> low ;
     std::vector<int> high;
-    for(int i=0;i<r;i++){
-        if(a[i] < pivot ){
+
+    for(int i=0;i<sz;i++){
+        if (a[i]<pivot){
             low.push_back(a[i]);
         }else if (a[i] > pivot){
             high.push_back(a[i]);
         }
     }
+
     int k = int(low.size());
-    if (idx < k){
-        return median_of_median(low,idx);
+    if (idx < k) {
+        return median_of_medians(low,idx);
+
     }else if (idx > k) {
-        return median_of_median(high,idx-k-1);
-    }else{
-        return pivot ;
+        return median_of_medians(high,idx-k-1);
+    }else {
+        return pivot;
     }
 }
 
 int main(){
-    std::vector<int> A{25,21,98,100,76,22,43,60,89,87};
-	int i = 3;
-    median_of_median(A,6);
+    std::vector<int> A{5,3,6,4,2,8,3,8,34,0};
+	int kth = 4;
+    //finding the kth smallesst elemnt 
+    std::cout << median_of_medians(A,kth) << std::endl ; 
 
 }
